5fa98f6d3fb006f8094b2fc7a9cbbc36a7ae638e
Cody Short <codycodes434@gmail.com>
2026-04-05 12:48:33 -0400

    test

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const path = require('path');
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import path from 'path';
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server, {
+const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"]
