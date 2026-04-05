02140ab8c67fb1294d725838dfcd7fc887fee136
Cody Short <codycodes434@gmail.com>
2026-04-05 12:57:51 -0400

    fix server issue

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const server = http.createServer(app);
